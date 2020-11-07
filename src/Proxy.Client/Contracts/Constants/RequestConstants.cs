0fbd8a87dbc894bd4c40ccabd3411c7e7918f6dc
Daniel <danzanzu@gmail.com>
2020-11-08 00:18:13 +0100

    ContentLength Pattern

--- src/Proxy.Client/Contracts/Constants/RequestConstants.cs
+++ src/Proxy.Client/Contracts/Constants/RequestConstants.cs
@@ -5,7 +5,7 @@
         internal const string CONTENT_SEPERATOR = "\r\n\r\n";
         internal const string CONTENT_LENGTH_HEADER = "Content-Length";
         internal const string TRANSFER_ENCODING_CHUNKED_HEADER = "chunked";
-        internal const string CONTENT_LENGTH_PATTERN = "(?<=Content-Length: ).\\d+";
+        internal const string CONTENT_LENGTH_PATTERN = "(?<=Content-Length: )[0-9]*";
         internal const string STATUS_CODE_PATTERN = "\\d\\d\\d";
         internal const string NO_SSL = "http";
         internal const string SSL = "https";
